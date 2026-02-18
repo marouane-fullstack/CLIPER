7b2718fbdf4cfb99054395e63e14858e91405995
marouane-fullstack <ramali.master2005@gmail.com>
2026-02-18 10:41:40 +0000

    theme (dark | light) integrated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider as NextThemesProvider,useTheme,type ThemeProviderProps } from 'next-themes'
-import { useEffect } from "react";
-import { dark } from '@clerk/themes'
+import { type ThemeProviderProps } from 'next-themes'
+import RootLayoutProvider from "@/components/RootLayoutProvider";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,31 +11,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, ...props }: Readonly<ThemeProviderProps>) {
 
-  const { resolvedTheme, setTheme } = useTheme()
-
-  useEffect(() => {
-    /* 
-     * If you have changed your storageKey in your <ThemeProvider storageKey="" />,
-     * make sure you change it in the localStorage.getItem too.
-     * default key is "theme"
-     */
-    const actualTheme = localStorage.getItem('your-storage-key-theme')
-    setTheme(actualTheme || 'system')
-  }, [setTheme])
   return (
-    <NextThemesProvider {...props} attribute="class" defaultTheme="system" enableSystem
-          disableTransitionOnChange >
-    <ClerkProvider appearance={{
-        baseTheme: resolvedTheme === 'dark' ? dark : undefined
-      }}>
-    <html lang="en"  suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-    </ClerkProvider>
-    </NextThemesProvider>
+    <RootLayoutProvider {...props}>
+    {children}
+    </RootLayoutProvider>
   );
 }
