fb1e2f027944b793b81625f4ee91cb10937c8cf2
marouane-fullstack <ramali.master2005@gmail.com>
2026-02-19 16:06:31 +0000

    change only the root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-import prisma from '@/lib/prisma';
 
 export default async function Home() {
-  // Database call
-  const users = await prisma.user.findMany();
+
   return (
     <main>
-      <h1>Users</h1>
-      {users.map((user) => (
-        <div key={user.id}>
-          {user.name} ({user.email})
-        </div>
-      ))}
     </main>
   );
 }
